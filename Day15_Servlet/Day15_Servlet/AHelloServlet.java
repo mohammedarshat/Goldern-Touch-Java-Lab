985ebfecf6ac0a2ea3ebe901d0fd4a66f641dc38
mohammedarshat <84720434+mohammedarshat@users.noreply.github.com>
2022-08-10 22:50:12 +0530

    Add files via upload

---
+++ Day15_Servlet/Day15_Servlet/AHelloServlet.java
@@ -0,0 +1,20 @@
+package Day15_Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AHelloServlet
+ */
+@WebServlet("/AHelloServlet")
+public class AHelloServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("First servlet method called program Bismillah....");
+	}
+
+}
