13e318417e33034b0870e45cb8df962b9866f52d
mohammedarshat <84720434+mohammedarshat@users.noreply.github.com>
2022-08-23 17:03:26 +0530

    Delete Aspire_Revision directory

--- Aspire_Revision/src/main/java/Day18_Login_Register/Action.java
+++
@@ -1,7 +0,0 @@
-package Day18_Login_Register;
-
-import javax.servlet.http.HttpServletRequest;
-
-public abstract class Action {
-	public abstract String execute(HttpServletRequest request , HttpServletRequest response);
-}
